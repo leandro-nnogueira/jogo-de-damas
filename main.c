89168e00f6aa6e5a0926e678a6e69d14e483f04e
Leandro Nogueira <157415649+leandro-nnogueira@users.noreply.github.com>
2024-10-16 22:39:04 +0000

    Add function to verify and place chess pieces on the board

--- main.c
+++ main.c
@@ -10,28 +10,37 @@ void mostrarTabuleiro(int tabuleiro[N][N]) {
     }
 }
 
-void colocarPeca(int tabuleiro[N][N]) {
-    int linha, coluna;
-
-    while (1) {
-
-    printf("Digite a linha e a coluna onde deseja colocar a peça: ");
-    scanf("%d %d", &linha, &coluna);
-
-    tabuleiro[linha][coluna] = 1;
-    printf("Peça colocada na linha %d e coluna %d\n", linha, coluna);
-
-    printf("Deseja colocar outra peça? (1 - Sim, 0 - Não): ");
-    int opcao;
-    scanf("%d", &opcao);
-    if (opcao == 0) {
-
-        break;
+void verificaPosicao(int tabuleiro[N][N], int linha, int coluna) {
+    if (tabuleiro[linha][coluna] == 1) {
+        printf("Posição ocupada, escolha outra\n");
     }
     else {
-        mostrarTabuleiro(tabuleiro);
+        tabuleiro[linha][coluna] = 1;
+        printf("Peça colocada na linha %d e coluna %d\n", linha, coluna);
     }
 }
+
+void colocarPeca(int tabuleiro[N][N]) {
+    int linha, coluna;
+    char continuar;
+
+    do {
+        mostrarTabuleiro(tabuleiro);
+
+        printf("Digite a linha e a coluna para colocar a peça\n");
+        scanf("%d %d", &linha, &coluna);
+
+        if (linha >= N || coluna >= N || linha < 0 || coluna < 0) {
+            printf("Posição inválida, fora do tabuleiro. Tente novamente.\n");
+        } else {
+            verificaPosicao(tabuleiro, linha, coluna);
+        }
+
+        mostrarTabuleiro(tabuleiro);
+
+        printf("Deseja continuar? (s/n): ");
+        scanf(" %c", &continuar);
+    } while (continuar == 's' || continuar == 'S');
 }
 
 int main() {
